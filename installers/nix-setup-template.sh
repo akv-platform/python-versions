57eb9d1a48968790bf6a6f253dfe8e06267ab4c3
MaksimZhukov <v-mazhuk@microsoft.com>
2020-07-17 13:11:05 +0300

    Remove PIP upgrading

--- installers/nix-setup-template.sh
+++ installers/nix-setup-template.sh
@@ -48,9 +48,5 @@ fi
 
 chmod +x ../python $PYTHON_MAJOR $PYTHON_MAJOR_DOT_MINOR $PYTHON_MAJORMINOR python
 
-echo "Upgrading PIP..."
-./python -m ensurepip
-./python -m pip install --ignore-installed pip
-
 echo "Create complete file"
 touch $PYTHON_TOOLCACHE_VERSION_PATH/x64.complete
