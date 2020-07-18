339c17188cb603aae78d6c5033c46db261577208
Maxim Lobanov <v-malob@microsoft.com>
2020-07-18 18:32:16 +0300

    Update nix-setup-template.sh

--- installers/nix-setup-template.sh
+++ installers/nix-setup-template.sh
@@ -49,8 +49,8 @@ fi
 chmod +x ../python $PYTHON_MAJOR $PYTHON_MAJOR_DOT_MINOR $PYTHON_MAJORMINOR python
 
 echo "Upgrading PIP..."
-./python -m ensurepip
-./python -m pip install --ignore-installed pip
+#./python -m ensurepip 2>&1
+./python -m pip install --ignore-installed pip 2>&1
 
 echo "Create complete file"
 touch $PYTHON_TOOLCACHE_VERSION_PATH/x64.complete
