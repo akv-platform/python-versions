7fdda28b33474cb43ea3445bae29a50c3e795072
Maxim Lobanov <v-malob@microsoft.com>
2020-07-18 07:25:10 +0300

    Update downgrade-open-ssl.sh

--- builders/downgrade-open-ssl.sh
+++ builders/downgrade-open-ssl.sh
@@ -1,13 +1,13 @@
 
 openssl version -a
 
-sudo dpkg -r --force-depends "openssl"
+#sudo dpkg -r --force-depends "openssl"
 
 echo "downloading"
 cd /usr/local/src/
-sudo wget https://www.openssl.org/source/openssl-1.1.1c.tar.gz
-sudo tar -xf openssl-1.1.1c.tar.gz
-cd openssl-1.1.1c
+sudo wget https://www.openssl.org/source/openssl-1.0.0.tar.gz
+sudo tar -xf openssl-1.0.0.tar.gz
+cd openssl-1.0.0
 
 echo "building"
 sudo ./config --prefix=/usr/local/ssl --openssldir=/usr/local/ssl shared zlib
@@ -15,7 +15,7 @@ sudo make
 sudo make test
 sudo make install
 cd /etc/ld.so.conf.d/
-sudo nano openssl-1.1.1c.conf
+sudo nano openssl-1.0.0.conf
 
 echo "result???"
 openssl version -a
